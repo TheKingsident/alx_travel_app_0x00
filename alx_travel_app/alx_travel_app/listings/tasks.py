108c0e20aeb8dfa16ab206a0f064030cf9ec4837
TheKingsident <kingsident@gmail.com>
2025-06-02 00:18:28 +0100

    Restructured

--- alx_travel_app/alx_travel_app/listings/tasks.py
+++
@@ -1,7 +0,0 @@
-# listings/tasks.py
-from celery import shared_task
-
-@shared_task
-def test_task():
-    print("Task executed")
-    return "Success"
--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'drf_yasg',
-    'alx_travel_app.listings',
+    'listings',
 ]
 
 MIDDLEWARE = [
